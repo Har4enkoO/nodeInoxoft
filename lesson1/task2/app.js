f18242afb5c6b31205069a879466b20b776bfd62
Har4enkoO <pan4enko48@gmail.com>
2021-08-19 16:33:58 +0300

    working on second task

---
+++ lesson1/task2/app.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+
+const renameFile = util.promisify(fs.rename);
+const readDirPromise = util.promisify(fs.readdir);
+const statPromisify = util.promisify(fs.stat);
+
+const mainFolder = path.join(__dirname);
+
+const changeFolder = async (folder) => {
+  try {
+    const data = await readDirPromise(folder);
+    data.forEach((file) => {
+      const currentFilePath = path.join(mainFolder, file);
+      statPromisify(currentFilePath, (err, stats) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        stats.isFile()
+          ? renameFile(currentFilePath, mainFolder)
+          : changeFolder(currentFilePath);
+      });
+    });
+  } catch (error) {
+    console.log("error ", error);
+  }
+};
+
+changeFolder(mainFolder);
